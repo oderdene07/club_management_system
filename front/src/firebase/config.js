831508d5cc280ee2728e5270742179f742eef66b
Od-Erdene <69565961+oderdene07@users.noreply.github.com>
2023-07-05 17:21:20 +0800

    Update config.js

--- front/src/firebase/config.js
+++ front/src/firebase/config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDXHg-H54vE3wpTuP90nDBsYGv8LH2kAGE",
-  authDomain: "club-management-system-d474b.firebaseapp.com",
-  projectId: "club-management-system-d474b",
-  storageBucket: "club-management-system-d474b.appspot.com",
-  messagingSenderId: "336669191655",
-  appId: "1:336669191655:web:fb3d0e1497704736e1e142",
-  measurementId: "G-GBE33NWRJD",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
